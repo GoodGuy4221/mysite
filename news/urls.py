c308c6c0a06386841dbbc50c790aaaeb8a916246
eric <_>
2022-02-21 14:46:18 +0400

    Параметры в URL-запросах.

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .apps import NewsConfig
-from .views import index
+from .views import index, get_category
 
 app_name = NewsConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
+    path('category/<int:category_id>/', get_category, name='get_category'),
 ]
