a235c68a0fe77463a6616cbd8423426e9b473473
eric <_>
2022-03-02 20:39:17 +0400

    and a little more work...

--- news/models.py
+++ news/models.py
@@ -9,13 +9,14 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория',
+                                 related_name='get_news')
 
     def get_absolute_url(self):
         return reverse(viewname='news:view_news', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return self.title
+        return f'{self.pk} {self.title}'
 
     class Meta:
         verbose_name = 'Новость'
@@ -30,7 +31,7 @@ class Category(models.Model):
         return reverse(viewname='news:category', kwargs={'category_id': self.pk})
 
     def __str__(self):
-        return self.title
+        return f'{self.pk} {self.title}'
 
     class Meta:
         verbose_name = 'Категория'
